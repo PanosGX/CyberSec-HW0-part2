6575f7f12d2059bcbacddf7285a22ee4c0f5e39a
PanosXalk <ginglapanos@gmail.com>
2026-03-18 19:24:28 +0200

    Vulnerable c program

--- Dockerfile
+++ Dockerfile
@@ -4,5 +4,5 @@ LABEL maintainer="PanosGX-Custom-Project"
 RUN apt-get update && apt-get install -y gcc libc6-dev && rm -rf /var/lib/apt/lists/*
 COPY buf_over.c /app/buf_over.c
 WORKDIR /app
-RUN gcc -o buf_over buf_over.c
-CMD ["./buf_over"]
+RUN gcc -fno-stack-protector -o buf_over buf_over.c
+CMD ["./buf_over", "AAAAAAAAAAAAAAAA"]
--- buf_over.c
+++ buf_over.c
@@ -1,7 +1,28 @@
 #include <stdio.h>
-int main()
+#include <string.h>
+
+int main(int argc, char *argv[])
 {
+    int pass = 0;
     char buffer[10];
-    printf("Program run normally\n");
+
+    if (argc < 2)
+    {
+        printf("Run program with a second argument\n");
+        printf("Like: ./buf_over Good Moring\n");
+        return 1;
+    }
+
+    strcpy(buffer, argv[1]);
+
+    if (pass != 0)
+    {
+        printf("WOW GG well done \n");
+    }
+    else
+    {
+        printf("Not quite, maybe try a bigger input\n");
+    }
+
     return 0;
 }
